d75183b3e0c47e9bd50748d7446a5e594ea1a5e9
david <davidd125@yahoo.com>
2019-08-24 21:43:36 +0100

    142. Added unit of Measure

--- recipeapp/src/main/java/com/djd2000/domain/Ingredient.java
+++ recipeapp/src/main/java/com/djd2000/domain/Ingredient.java
@@ -3,10 +3,12 @@ package com.djd2000.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /**
  * 
@@ -23,7 +25,8 @@ public class Ingredient {
 	String description;
 	BigDecimal amount;
 
-//	private UnitOfMeasure uom;
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
 	
 	@ManyToOne
 	private Recipe recipe;
@@ -58,6 +61,14 @@ public class Ingredient {
 
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
 	}
 
 }
--- recipeapp/src/main/java/com/djd2000/domain/Notes.java
+++ recipeapp/src/main/java/com/djd2000/domain/Notes.java
@@ -3,6 +3,7 @@ package com.djd2000.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
@@ -12,6 +13,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Notes {
 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
