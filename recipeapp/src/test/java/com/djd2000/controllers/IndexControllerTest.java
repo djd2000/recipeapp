d0d44fd3689f4f5742cf0344b25e6bc57a38b5b6
david <davidd125@yahoo.com>
2019-09-01 18:06:38 +0100

    183. Added Mockito Argument Capture

--- recipeapp/src/test/java/com/djd2000/controllers/IndexControllerTest.java
+++ recipeapp/src/test/java/com/djd2000/controllers/IndexControllerTest.java
@@ -3,15 +3,22 @@ package com.djd2000.controllers;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.djd2000.domain.Recipe;
 import com.djd2000.services.RecipeService;
 
 public class IndexControllerTest {
@@ -33,11 +40,26 @@ public class IndexControllerTest {
 	@Test
 	public void test() {
 
-		assertEquals(indexController.getIndexPage(model), "index");
-		// verify interactions in mocks
-		verify(recipeService, times(1)).getRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		// given
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		
+		Recipe recipe2 = new Recipe();
+		recipe2.setId(1L);
+		recipes.add(recipe2);
 
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+		// when
+		String viewName = indexController.getIndexPage(model);
+
+		// then
+		assertEquals("index", viewName);
+		verify(recipeService, times(1)).getRecipes();
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(2, setInController.size());
 	}
 
 }
